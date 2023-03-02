68b9186f66b2e9df7740da85c126bc4c77042071
Bohdan Shevchuk <shevdan007@gmail.com>
2023-03-02 23:03:55 +0200

    Parsed player tshirt numbers

--- src/player_page.py
+++ src/player_page.py
@@ -1,10 +1,11 @@
 from page import PagePath
-
+import bs4
+from collections import defaultdict
 
 class AbstractPage:
     def __init__(self, pagepath: PagePath) -> None:
         self.pagepath = pagepath
-        self._stats = {}
+        self._stats = defaultdict(str)
         self._naming_correspondence = {}
         self.soup = self._get_soup()
 
@@ -20,3 +21,23 @@ class AbstractPage:
     def get_stats(self):
         raise NotImplementedError("To be written in inherited classes")
 
+
+class TeamPage(AbstractPage):
+    def __init__(self, pagepath: PagePath) -> None:
+        super().__init__(pagepath)
+    
+    def get_stats(self):
+        def cond(x):
+            if x:
+                return "odd" in x or "even" in x
+            return False
+
+        rows = self.soup.findAll('tr', {'class': cond})
+        print(self.pagepath.curr_page_info, self.pagepath.page_date)
+        # print(rows)
+        for row in rows:
+            team_shirts = [shirt.text for shirt in row.findAll("div", {"class": "tm-shirt-number"}) if shirt.text]
+            player_names = [player.text for player in row.findAll("span", {"class": "hide-for-small"}) if player.text]
+            for i in range(len(team_shirts)):
+                self._stats[team_shirts[i]] = player_names[i]
+        # print(self.stats)
--- src/scrapper.py
+++ src/scrapper.py
@@ -11,6 +11,9 @@ import json
 import collections
 import re
 from page import PagePath
+from player_page import TeamPage
+from collections import defaultdict
+import bs4
 
 
 MAX_TIMER = 60
@@ -20,7 +23,7 @@ REFRESH_RATE = 14
 FILENAME_DATE_SEP = "№"
 
 class Player:
-    def __init__(self, url: str, project_dir = ".", verbose=False, outname="") -> None:
+    def __init__(self, url: str, player_name: str = "team", project_dir = ".", verbose=False, outname="") -> None:
         """
         Initial url obtained from google docs should be in form 
         https://www.hltv.org/player/3741/player_name
@@ -30,13 +33,13 @@ class Player:
             raise ValueError("Wrong url Error.")
 
         self.url = url
-        self.player_name = ""
+        self.player_name = player_name
         if not os.path.isdir(project_dir):
             raise ValueError("Wrong project directory path: Not a directory")
         self.project_dir = project_dir
         self.verbose = verbose
         self.outname = outname
-        self.url_dict = {}
+        self.url_dict = defaultdict(str)
 
         if not os.path.exists(self.project_dir):
             os.mkdir(self.project_dir)
@@ -48,6 +51,8 @@ class Player:
             os.mkdir(player_path)
         
         self._get_urls()
+        self.update_pages()
+
         
         self.pages = []
         
@@ -57,10 +62,54 @@ class Player:
         }
 
         
-        
+
     
     def _get_urls(self):
-        raise NotImplementedError("")
+        # stats = requests.get(self.url, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' } )
+        # html_text = stats.text
+        # # print(html_text)
+        # with open("br_leage.html", mode="w") as f:
+        #     f.write(html_text)
+        with open("br_leage.html", mode="r") as f:
+            html_text = f.read()
+        soup = bs4.BeautifulSoup(html_text, "html.parser")
+        # lst = [t for t in soup.find_all("tr") if not isinstance(t, bs4.element.NavigableString)]
+        # # for elm in lst:
+        # print(lst, len(lst))
+        tds = []
+        # soup = BeautifulSoup(page_src, 'html.parser')
+        divs = soup.findAll("span", {"class": "vereinsname"})
+        # print(set(divs))
+        # print(divs)
+
+        for span in divs:
+            # print(div.contents)
+            links = span.find_all('a')
+            for link in links:
+                if "saison" in link["href"]:
+                    l = link['href'].replace("spielplan", "startseite")
+                    self.url_dict[link.text] = f"https://www.transfermarkt.com{l}"
+        pprint(self.url_dict)
+        #     rows = div.findAll('tr')
+        #     print(rows)
+        #     for row in rows :
+        #         tds.append(row.findAll('td'))
+        # print(tds)
+        # for tag in soup.find_all("td", {"class": "hauptlink no-border-links"}):
+        #     for info in tag.contents:
+                # if  isinstance(info, bs4.element.NavigableString):
+                #     continue
+                
+                # print(info.text)
+            
+                # for key in self._stats.keys():
+                #     if info.text.strip() in key and not info.text.strip().isdigit() :
+                #         self._stats[f"{self.pagepath.curr_page_info}_{info.text.strip()}"] = stat_value
+                #         continue
+
+                # stat_value = info.text.strip()
+                # if stat_value == "-":
+                #     stat_value = "0"
 
        
 
@@ -76,14 +125,14 @@ class Player:
 
     def save_html(self, url, tag, timer=1):
         
-        if not self.to_refresh(tag):
-            return
+        # if not self.to_refresh(tag):
+        #     return
         
         curr_date = date.today()
 
         try:
             time.sleep(timer)
-            stats = requests.get(url)
+            stats = requests.get(url, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' })
         except requests.exceptions.Timeout:
             if timer > MAX_TIMER:
                 if not os.path.exists(f"{self.project_dir}{os.path.sep}log"):
@@ -106,7 +155,16 @@ class Player:
             f.write(html_text)
 
     def update_pages(self):
-        raise NotImplementedError("")
+        player_path = f"{self.project_dir}{os.path.sep}data{os.path.sep}{self.player_name}"
+        self.pages = [ pagefile for pagefile in os.listdir(player_path)]
+        
+
+        self.pages = [PagePath(f"{player_path}{os.path.sep}{pagefile}") for pagefile in self.pages]
+        #sort pages by date
+        self.pages = sorted(self.pages, reverse=True, key=lambda x: x.page_date)
+        #get the most recent pages that are necessary for getting whole stat
+        # self.pages = self.pages[:len(self.url_dict)]
+
 
     def cache_all_pages(self):
         for tag, url in self.url_dict.items():
@@ -120,7 +178,7 @@ class Player:
 
 
     def get_stats(self):
-        all_stats = {}
+        all_stats = defaultdict(str)
 
         num_page = 1
 
@@ -128,10 +186,11 @@ class Player:
             if self.verbose:
                 print(f"\nScrapping. Current page: {page.curr_page_info}. Page number: {num_page}")
             num_page += 1
-            curr_page = self._page_correspondence[page.stat](page)
+            curr_page = TeamPage(page)
             curr_page.get_stats()
-            all_stats.update(curr_page.stats)
-        return all_stats
+            # print(curr_page.stats)
+            all_stats[page.curr_page_info] = dict(curr_page.stats)
+        return dict(all_stats)
         
     def save_stats(self, all_stats):
         if not self.outname:
@@ -146,6 +205,15 @@ class Player:
         od = collections.OrderedDict(sorted(all_stats.items()))
         if self.verbose:
             print(f"Saving stats to {results_dir}{os.path.sep}{self.outname}")
-        with open(f"{results_dir}{os.path.sep}{self.outname}", mode="w") as f:
+        with open(f"{results_dir}{os.path.sep}{self.outname}", mode="w", encoding="utf-8") as f:
             json.dump(od, f)
     
+
+if __name__ == "__main__":
+    gb_pl = Player(url = "https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1", player_name="Laliga")
+    # gb_pl.cache_all_pages()
+    gb_pl.update_pages()
+    print(gb_pl.pages)
+    stats = gb_pl.get_stats()
+    pprint(stats)
+    gb_pl.save_stats(stats)
